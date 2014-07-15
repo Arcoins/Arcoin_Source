89a97e9db7513a8fa9af43154594ccaab7707298
ARCoin <arcoins@outlook.com>
2014-07-15 22:42:54 +0200

    Source

---
+++ src/onionseed.h
@@ -0,0 +1,18 @@
+
+#ifndef AR_ONIONSEED_H
+#define AR_ONIONSEED_H
+
+// hidden service seeds
+static const char *strMainNetOnionSeed[][1] = {
+    {"mwao22u7uzpzsqvj.onion"},
+    {"2jdh6xo55oqsydat.onion"},
+    {NULL}
+};
+
+static const char *strTestNetOnionSeed[][1] = {
+    {"mwao22u7uzpzsqvj.onion"},
+    {"2jdh6xo55oqsydat.onion"},
+    {NULL}
+};
+
+#endif
